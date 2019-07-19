179595d9371bdf37711247d0c4408b2146b79544
Lee Jin Yao <35756209+jylee-git@users.noreply.github.com>
2019-07-19 15:24:23 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::cout << "Hello" << endl;
+    
+    //@@author successfulAuthorTag
+    
+    std::cout << "Hello" << endl;
+    
+    //@@author 
+    
+    std::cout << "Hello" << endl;
+    
+    return 0;
+}
